21bf43827dfc1b688f7ed0f5acc429421deae5d7
Hisham Malik <hisham@arkhitech.com>
2025-07-09 04:48:03 +0500

    fix render recursion issue, skip if app.app does not respond to routes method, include mcp concerns in ActionController::API

--- lib/mcp/rails/server_generator/route_collector.rb
+++ lib/mcp/rails/server_generator/route_collector.rb
@@ -5,7 +5,7 @@ module MCP
         routes.map do |route|
           app = route.app.app
 
-          if app.respond_to?(:routes) && app < ::Rails::Engine
+          if app.respond_to?(:routes) && app < ::Rails::Engine && app.app.respond_to?(:routes)
             new_prefix = [ prefix, route.path.spec.to_s ].join
             collect_routes(app.app.routes.routes, new_prefix, app)
           else
@@ -39,7 +39,7 @@ module MCP
             action = route.defaults[:action].to_sym
             params_def = controller_class.permitted_params(action)
           rescue NameError => e
-            warn("Controller not found for route: #{route.defaults[:controller]}; #{e.message}")
+            warn("Error for #{route.defaults[:controller]}:#{route.defaults[:action]}; #{e.message}")
             next
           end
 
